30e609ce4ba8b3ab30fe09dde0f225224f9a888d
wrobel2131 <wrobelek2131@wp.pl>
2023-02-16 22:52:27 +0100

    Filled sections on the home page, changed user info bya dding button and displaying alerts

--- beJacked-angular/src/app/modules/home/components/home/home.component.ts
+++ beJacked-angular/src/app/modules/home/components/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
+  contactForm!: FormGroup;
+  faFacebook = faFacebook;
+  faGithub = faGithub;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      email: this.fb.control(null, [Validators.required]),
+      message: this.fb.control(null, [Validators.required]),
+    });
+  }
 }
